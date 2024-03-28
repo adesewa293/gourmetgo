102e2194618c06d5113565525212620c28534b10
Adesewa Aishat <adesewaaishat@gmail.com>
2024-03-28 22:42:16 +0000

    update hero section

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -1,21 +1,25 @@
 import Image from "next/image";
+import Right from "../icons/Right";
 
 export default function Hero() {
   return (
-    <section className="grid grid-cols-2">
-      <div>
-      <h1 className="text-4xl font-semibold">Everything is better with food</h1>
-      <p className="my-4 text-gray-500">
-        Indulge in exquisite flavors, artisanal ingredients, and culinary
-        artistry. Our gourmet food is meticulously crafted, offering a symphony
+    <section className="hero">
+      <div className="py-12">
+      <h1 className="text-4xl font-semibold">Everything<br /> is better <br/>with&nbsp;<span className="text-primary">Gourmet Food</span></h1>
+      <p className="my-6 text-gray-500 text-sm">
+        Our gourmet food is meticulously crafted, offering a symphony
         of taste, texture, and presentation for discerning palates to savor and
         celebrate.
       </p>
-      <div>
-        <button className="bg-primary text-white px-8 py-2 rounded-full">
+      <div className="flex gap-4 text-sm">
+        <button className="bg-primary uppercase items-center flex gap-2 text-white px-8 py-2 rounded-full">
           Order now
+          <Right />
         </button>
-        <button>Learn more</button>
+        <button className="flex gap-2 py-2 text-gray-600 font-semibold items-center">
+          Learn more
+          <Right />
+          </button>
       </div>
       </div>
       
