947866c8a39947514a6a5e8ec9f14b189a1df1d0
Adesewa Aishat <adesewaaishat@gmail.com>
2024-03-28 20:57:38 +0000

    update header

--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,12 @@ export default function Home() {
   <>
   <header className="flex items-center justify-between">
     <Link className="text-primary font-semibold text-2xl" href="">GOURMET GO</Link>
-    <nav className="flex gap-4">
+    <nav className="items-center flex gap-8 text-gray-500 font-semibold">
       <Link href="">Home</Link>
       <Link href="">Menu</Link>
       <Link href="">About</Link>
       <Link href="">Contact</Link>
-      <Link href="">Login</Link>
+      <Link href="" className="bg-primary text-white rounded-full px-8 py-2">Login</Link>
     </nav>
   </header>
   </>
