d3492e6bbeb9331e3355da7cb70ef815dedfe9db
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-08 18:19:40 +0100

    adjust register page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -9,17 +9,24 @@ export default function RegisterPage() {
   const [password, setPassword] = useState("");
   const [creatingUser, setCreatingUser] = useState(false);
   const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
   async function handleFormSubmit(event) {
     event.preventDefault();
     setCreatingUser(true);
-    await fetch("/api/register", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-    setCreatingUser(false);
-    setUserCreated(true);
-  }
+    setError(false);
+    setUserCreated(false);
+      const response = await fetch("/api/register", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        setUserCreated(true);
+      } else {
+        setError(true);
+      }
+      setCreatingUser(false);
+    }
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
@@ -30,6 +37,11 @@ export default function RegisterPage() {
             Login &raquo;{" "}
           </Link>
         </div>
+      )}
+      {error && (
+         <div className="my-4 text-center">
+         An error occured. <br /> Please try again later
+       </div>
       )}
       <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
         <input
@@ -61,6 +73,9 @@ export default function RegisterPage() {
           />
           Login with google
         </button>
+        <div className="text-center my-4 text-gray-500 border-t pt-4">
+          Already have an account?{' '} <Link className="underline" href={'/login'}>login here &raquo; </Link>
+        </div>
       </form>
     </section>
   );
