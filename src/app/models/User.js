cd77ce31fa0909f785d6639a78b9b30c1b046a19
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-08 17:39:54 +0100

    update authentication

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import { model, models, Schema } from "mongoose";
+import bcrypt from 'bcryptjs';
 
 const UserSchema = new Schema({
   email: { type: String, required: true, unique: true },
@@ -13,5 +14,11 @@ const UserSchema = new Schema({
     },
   },
 },{timestamps: true});
+
+UserSchema.post('validate', function(user) {
+  const notHashedPassword = user.password;
+  const salt = bcrypt.genSaltSync(10);
+  user.password = bcrypt.hashSync(notHashedPassword, salt);
+})
 
 export const User = models?.User || model("User", UserSchema);
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,37 +1,54 @@
 "use client";
 
 import Image from "next/image";
+import Link from "node_modules/next/link";
 import { useState } from "react";
-
 
 export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  function handleFormSubmit(event){
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  async function handleFormSubmit(event) {
     event.preventDefault();
-    fetch('/api/register', {
-      method: 'POST',
-      body: JSON.stringify({email, password}),
-      headers: {'Content-Type': 'application/json'},
-    })
-    }
+    setCreatingUser(true);
+    await fetch("/api/register", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    setCreatingUser(false);
+    setUserCreated(true);
+  }
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created. <br /> now you can{" "}
+          <Link className="underline" href={'/login'}>
+            Login &raquo;{" "}
+          </Link>
+        </div>
+      )}
       <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
         <input
           type="email"
           placeholder="email"
           value={email}
+          disabled={creatingUser}
           onChange={(event) => setEmail(event.target.value)}
         />
         <input
           type="password"
           placeholder="password"
           value={password}
+          disabled={creatingUser}
           onChange={(event) => setPassword(event.target.value)}
         />
-        <button type="submit">Register</button>
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
         <div className="my-4 text-center text-gray-500">
           or login with provider
         </div>
