1ca2002d7da10676321d053bb74129b82591ba88
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-09 17:14:32 +0100

    update login authentication

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,25 +1,33 @@
+import mongoose from "mongoose";
+import { User } from "@/app/models/User";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
+  secret: process.env.SECRET,
   providers: [
     CredentialsProvider({
       name: "Credentials",
-
+      id: "credentials",
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "test@example.com",
+        },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+        const email = credentials?.email;
+        const password = credentials?.password;
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
 
-        if (user) {
+        if (passwordOk) {
           return user;
-        } else {
-          return null;
-
-          parameter;
         }
+        return null;
       },
     }),
   ],
