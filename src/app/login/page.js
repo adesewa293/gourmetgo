a57502d061dcfd5509dc9d802d5a93b0a76179f3
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-09 13:12:58 +0100

    update login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,30 +1,57 @@
-"use client"
+"use client";
 
 import { useState } from "react";
+import Image from "node_modules/next/image";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loginInProgress, setLoginInProgress] = useState(false);
+  async function handleFormSubmit(event) {
+    event.preventDefault();
+    setLoginInProgress(true);
+    const { ok } = await fetch("/api/login", {
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+      method: "POST",
+    });
+    if (ok) {
+    } else {
+    }
+    setLoginInProgress(false);
+  }
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Login</h1>
-    <form>
-    <input
+      <form className="max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+        <input
           type="email"
           placeholder="email"
           value={email}
-          disabled={false}
+          disabled={loginInProgress}
           onChange={(event) => setEmail(event.target.value)}
         />
         <input
           type="password"
           placeholder="password"
           value={password}
-          disabled={false}
+          disabled={loginInProgress}
           onChange={(event) => setPassword(event.target.value)}
         />
-    </form>
-
+        <button disabled={loginInProgress}  type="submit">Login</button>
+        <div className="my-4 text-center text-gray-500">
+          or login with provider
+        </div>
+        <button className="flex gap-4 justify-center">
+          <Image
+            src={"/google-icon.png"}
+            alt={"google-icon"}
+            width={24}
+            height={24}
+          />
+          Login with google
+        </button>
+      </form>
     </section>
   );
 }
