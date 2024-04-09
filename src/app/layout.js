7cd9c97bd700deb1ab2c7926881a22c7a6015e86
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-09 17:14:00 +0100

    add session provider and update layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/layout/Header";
+import { AppProvider } from "@/components/AppContext";
 
-const roboto = Roboto({ subsets: ["latin"], weight: ['400', '500', '700'] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "500", "700"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -14,13 +15,15 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
-          <Header />
-          {children}
-          <footer className="border-t p-8 text-center text-gray-500 mt-16">
-       &copy; 2024 All rights reserved
-      </footer>
-          </main>
-        </body>
+          <AppProvider>
+            <Header />
+            {children}
+            <footer className="border-t p-8 text-center text-gray-500 mt-16">
+              &copy; 2024 All rights reserved
+            </footer>
+          </AppProvider>
+        </main>
+      </body>
     </html>
   );
 }
