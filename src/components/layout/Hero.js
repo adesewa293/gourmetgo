5d64bf19db4aba550482cdd233cbc55b92ed56cf
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-15 12:56:43 +0100

    adjust order button

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
         celebrate.
       </p>
       <div className="flex gap-4 text-sm">
-        <button className="bg-primary uppercase items-center flex gap-2 text-white px-8 py-2 rounded-full">
+        <button className="bg-primary uppercase items-center flex gap-2 text-white px-4 py-2 rounded-full justify-center">
           Order now
           <Right />
         </button>
