8e201c6a6b5fbd6af543ad42eba2f2b37f6b5203
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-15 13:25:57 +0100

    update login button

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -41,7 +41,7 @@ export default function LoginPage() {
         <div className="my-4 text-center text-gray-500">
           or login with provider
         </div>
-        <button
+        <button type="button"
           onClick={() => signIn("google", {callbackUrl:'/'})}
           className="flex gap-4 justify-center"
         >
