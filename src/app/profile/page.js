bf555c1b853cc406aed385a809757be223bd27a6
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-19 15:46:58 +0100

    update profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,29 +1,64 @@
-"use client"
-
+"use client";
 
 import { useSession } from "next-auth/react";
 import { redirect } from "next/dist/server/api-utils";
-
-
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function profilePage() {
   const session = useSession();
-  const {status} = session;
-  if (status === 'loading') {
-    return 'Loading...'
+  const [userName, setUserName] = useState('');
+  const { status } = session;
+
+useEffect(() => {
+  if (status === 'authenticated'){
+    setUserName(session.data.user.name)
   }
-  if (status === 'unauthenticated') {
-    return redirect('/login');
+}, [session, status])
+
+
+async function handleProfileInfoUpdate(event){
+event.preventDefault();
+const response = await fetch('/api/profile', {
+  method: 'PUT',
+  headers: {'Content-Type': 'application/json'},
+  body: JSON.stringify({name:userName}),
+})
+}
+
+
+  if (status === "loading") {
+    return "Loading...";
+  }
+  if (status === "unauthenticated") {
+    return redirect("/login");
   }
   const userImage = session.data.user.image;
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Profile</h1>
-      <form className="max-w-xs mx-auto border">
-        <div>
-          <Image src={userImage} width={64} height={64} alt={avatar} />
+      <div className="max-w-md mx-auto">
+        <div className="flex gap-4 items-center">
+          <div>
+            <div className="p-2 rounded-lg relative">
+              <Image
+                className="rounded-lg w-full h-full mb-1"
+                src={userImage}
+                width={250}
+                height={250}
+                alt={"avatar"}
+              />
+
+              <button type="button">Edit</button>
+            </div>
+          </div>
+          <form className="grow" onSubmit={handleProfileInfoUpdate}>
+            <input type="text" placeholder="first and lastname" value={userName} onChange={event => setUserName(event.target.value)}/>
+            <input type="email" disabled={true} value={session.data.user.email} />
+            <button type="submit">Save</button>
+          </form>
         </div>
-      </form>
+      </div>
     </section>
   );
 }
