3bc67d68cb051910fafbd0d21082e790dc10c4d3
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-23 15:52:35 +0100

    add update feature to profile page

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 import { authOptions } from "../auth/[...nextauth]/route";
 import { User } from "@/app/models/User";
+import { getServerSession } from "next-auth";
 
 export async function PUT(req){
 mongoose.connect(process.env.MONGO_URL);
@@ -10,6 +11,7 @@ const email = session.user.email;
 
 if ('name' in data) {
 await User.updateOne({email}, {name:data.name});
+
 }
 return Response.json(true);
 }
--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,24 +1,13 @@
 import { model, models, Schema } from "mongoose";
-import bcrypt from 'bcryptjs';
+
 
 const UserSchema = new Schema({
+  name: {type: String},
   email: { type: String, required: true, unique: true },
   password: {
     type: String,
-    required: true,
-    validate: (pass) => {
-      if (!pass?.length || pass.length < 5) {
-        new Error("password must be at least 5 characters");
-        return false;
-      }
-    },
   },
 },{timestamps: true});
 
-UserSchema.post('validate', function(user) {
-  const notHashedPassword = user.password;
-  const salt = bcrypt.genSaltSync(10);
-  user.password = bcrypt.hashSync(notHashedPassword, salt);
-})
 
 export const User = models?.User || model("User", UserSchema);
