2942e1f5aa4606cfc21b3cbceea7d4fda512250b
Adesewa Aishat <adesewaaishat@gmail.com>
2024-04-25 22:25:27 +0100

    update upload API route

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,7 +1,3 @@
 export async function POST(req) {
-  const data = await req.formData();
-  if (data.get('file')){
-console.log('we have file', data.get('file'));
-  }
   return Response.json(true);
 }
