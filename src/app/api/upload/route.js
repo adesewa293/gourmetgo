be28593a1ce9b78701b457697f69218abb36121e
Adesewa Aishat <adesewaaishat@gmail.com>
2024-05-06 20:14:27 +0100

    add upload api-route

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,3 +1,41 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import uniqid from 'uniqid';
+
 export async function POST(req) {
+  const data = await req.formData();
+  console.log('data', data, data.get("file"))
+
+  if (data.get("file")) {
+    const file = data.get("file");
+   
+    const s3Client = new S3Client({
+      region: "us-east-1",
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY,
+      },
+    });
+    const extension = file.name.split('.').slice(-1)[0];
+    const newFileName = uniqid() + '.' + extension;
+
+    const chunks = [];
+    for await (const chunk of file.stream()){
+      chunks.push(chunk);
+    }
+    const buffer = Buffer.concat(chunks);
+
+
+    const bucket = 'gourmetgo';
+
+    await s3Client.send(new PutObjectCommand({
+      Bucket: bucket,
+      Key: newFileName,
+      ACL: 'public_read',
+      ContentType: file.type,
+      Body: buffer,
+    }));
+    const link = 'https://'+bucket+'.s3.amazonaws.com/'+newFileName;
+    return Response.json(link)
+  }
   return Response.json(true);
 }
