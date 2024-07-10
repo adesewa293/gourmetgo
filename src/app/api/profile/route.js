0625be62c1f0d280f2b5c82329b6b6a9d957cb09
Adesewa Aishat <adesewaaishat@gmail.com>
2024-07-10 20:56:07 +0100

    update profile api route

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -2,14 +2,21 @@ import mongoose from "mongoose"
 import { authOptions } from "../auth/[...nextauth]/route";
 import { User } from "@/app/models/User";
 import { getServerSession } from "next-auth";
+import { UserInfo } from "@/app/models/UserInfos";
 
 export async function PUT(req){
 mongoose.connect(process.env.MONGO_URL);
 const data = await req.json();
+const {name, image, ...otherUserInfo} =data;
+
 const session = await getServerSession(authOptions);
 const email = session.user.email;
 
-await User.updateOne({email}, data);
+
+
+await User.updateOne({email}, {name,image});
+await UserInfo.findOneAndUpdate({email}, otherUserInfo, {upsert:true});
+
 return Response.json(true);
 }
 
@@ -20,7 +27,8 @@ const email = session?.user?.email;
 if (!email) {
 return Response.json({})
 }
-return Response.json(
- await User.findOne({email})
-);
+const user = await User.findOne({email}).lean();
+const userInfo = await UserInfo.findOne({email}).lean();
+
+return Response.json({...user, ...userInfo});
 }
