cfbef60e8525cc45689dfdcf34e6d589823cd358
Adesewa Aishat <adesewaaishat@gmail.com>
2024-07-12 00:06:50 +0100

    update editing categories on categories page

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -5,40 +5,42 @@ import UserTabs from "@/components/layout/UserTabs";
 import toast from "react-hot-toast";
 
 export default function CategoriesPage() {
-  const [newCategoryName, setNewCategoryName] = useState('');
+  const [CategoryName, setCategoryName] = useState("");
   const [categories, setCategories] = useState([]);
   const { loading: profileLoading, data: profileData } = useProfile();
+  const [editedCategory, setEditedCategory] = useState(null);
 
-  useEffect(() =>{
-  fetchCategories();
-  }, [])
+  useEffect(() => {
+    fetchCategories();
+  }, []);
 
-function fetchCategories(){
-  fetch('/api/categories').then(res => {res.json().then(categories => {
-    setCategories(categories)
-  });
-});
-}
+  function fetchCategories() {
+    fetch("/api/categories").then((res) => {
+      res.json().then((categories) => {
+        setCategories(categories);
+      });
+    });
+  }
 
-async function handleNewCategorySubmit(ev){
-ev.preventDefault();
-const creationPromise = new Promise(async (resolve, reject) => {
-  const response = await fetch('/api/categories', {
-    method:'POST',
-    headers: {'Content-Type': 'application/json' },
-    body: JSON.stringify({name: newCategoryName}),
-  });
-  fetchCategories();
-  if (response.ok)
-    resolve();
-  else reject();
-});
-await toast.promise(creationPromise, {
-loading: 'Creating your new category...',
-success: 'Category created',
-error: 'Error, sorry...',
-});
-}
+  async function handleNewCategorySubmit(ev) {
+    ev.preventDefault();
+    const creationPromise = new Promise(async (resolve, reject) => {
+      const response = await fetch("/api/categories", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name: categoryName }),
+      });
+      setCategoryName("");
+      fetchCategories();
+      if (response.ok) resolve();
+      else reject();
+    });
+    await toast.promise(creationPromise, {
+      loading: "Creating your new category...",
+      success: "Category created",
+      error: "Error, sorry...",
+    });
+  }
 
   if (profileLoading) {
     return "Loading user info ...";
@@ -53,24 +55,37 @@ error: 'Error, sorry...',
       <form className="mt-8" onSubmit={handleNewCategorySubmit}>
         <div className="flex gap-2 items-end">
           <div className="grow">
-            <label>New category name</label>
-            <input type="text" value={newCategoryName} onChange={ev => setNewCategoryName(ev.target.value)}/>
+            <label>
+              {editedCategory ? "Update Category" : "New category name"}
+              {editedCategory && <>: <b>{editedCategory.name}</b></>}
+            </label>
+            <input
+              type="text"
+              value={CategoryName}
+              onChange={(ev) => setCategoryName(ev.target.value)}
+            />
           </div>
           <div className="pb-2">
-            <button className="border border-primary" type="submit">Create</button>
+            <button className="border border-primary" type="submit">
+              {editedCategory ? "Update" : "Create"}
+            </button>
           </div>
         </div>
       </form>
-     <div>
-      <h2 className="mt-8 text-sm text-gray-500">Edit category:</h2>
-      {categories?.length > 0 && categories.map(c => (
-      <div  className="bg-gray-200 rounded-xl p-2 px-4 flex gap-1 cursor-pointer mb-2">
-        <span>
-        {c.name}
-        </span>
+      <div>
+        <h2 className="mt-8 text-sm text-gray-500">Edit category:</h2>
+        {categories?.length > 0 &&
+          categories.map((c) => (
+            <button
+              onClick={() => {setEditedCategory(c);
+              setCategoryName(c.name);
+              }}
+              className="bg-gray-200 rounded-xl p-2 px-4 flex gap-1 cursor-pointer mb-1"
+            >
+              <span>{c.name}</span>
+            </button>
+          ))}
       </div>
-      ))}
-     </div>
     </section>
   );
 }
