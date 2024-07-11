e43bde1c704429494ba557192cf37f05ba0c403c
Adesewa Aishat <adesewaaishat@gmail.com>
2024-07-11 23:00:44 +0100

    add categories form to categories page

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,24 +1,48 @@
-'use client'
-
+"use client";
+import { useState } from "react";
 import { useProfile } from "@/components/UseProfile";
 import UserTabs from "@/components/layout/UserTabs";
 
-
 export default function CategoriesPage() {
-  
-  const {loading:profileLoading, data:profileData} = useProfile();
+  const [newCategoryName, setNewCategoryName] = useState('');
+  const { loading: profileLoading, data: profileData } = useProfile();
 
-  if (profileLoading){
-    return 'Loading user info ...';
+async function handleNewCategorySubmit(ev){
+ev.preventDefault();
+const creationPromise = new Promise(async (resolve, reject) => {
+  const response = await fetch('/api/categories', {
+    method:'POST',
+    headers: {'Content-Type': 'application/json' },
+    body: JSON.stringify({name: newCategoryName}),
+  });
+  if (response.ok)
+    resolve();
+  else reject();
+});
+
+}
+
+  if (profileLoading) {
+    return "Loading user info ...";
   }
-  if (!profileData.admin){
-return 'Not an admin';
+  if (!profileData.admin) {
+    return "Not an admin";
   }
 
   return (
-    <section className="mt-8 max-w-lg mx-auto">
+    <section className="mt-8 max-w-md mx-auto">
       <UserTabs isAdmin={true} />
-      categories
+      <form className="mt-8" onSubmit={handleNewCategorySubmit}>
+        <div className="flex gap-2 items-end">
+          <div className="grow">
+            <label>New category name</label>
+            <input type="text" value={newCategoryName} onChange={ev => setNewCategoryName(ev.target.value)}/>
+          </div>
+          <div className="pb-2">
+            <button className="border border-primary" type="submit">Create</button>
+          </div>
+        </div>
+      </form>
     </section>
   );
 }
