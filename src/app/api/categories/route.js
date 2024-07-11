a8faff10847fbdd04d7e8fdf58fd87b313de3904
Adesewa Aishat <adesewaaishat@gmail.com>
2024-07-11 23:15:36 +0100

    add categories route

---
+++ src/app/api/categories/route.js
@@ -0,0 +1,8 @@
+import { Category } from "@/app/models/Category";
+ 
+ export async function POST(req) {
+   const {name} = await req.json();
+   const categoryDoc = await Category.create({name});
+   return Response.json(categoryDoc );
+ }
+ 
