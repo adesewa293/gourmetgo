66f280aec05892be3e732f3e341f26735316688d
Adesewa Aishat <adesewaaishat@gmail.com>
2024-07-26 14:38:40 +0100

    add menu-items page

---
+++ src/app/menu-items/page.js
@@ -0,0 +1,22 @@
+"use client";
+
+import { useProfile } from "@/components/UseProfile";
+import UserTabs from "@/components/layout/UserTabs";
+
+export default function MenuItemsPage() {
+
+  const {loading, data} = useProfile();
+
+  if (loading) {
+  return 'Loading user info ...';
+  }
+  if (!data.admin) {
+    return 'Not an admin';
+  }
+
+  return (
+    <section className="mt-8">
+      <UserTabs isAdmin={true}/>
+    </section>
+  );
+}
