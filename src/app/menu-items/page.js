bdec3717adccef145900fecf4d2a0fbf8b5f3d65
Adesewa Aishat <adesewaaishat@gmail.com>
2024-07-31 17:03:42 +0100

    update menu-items page

--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -17,6 +17,17 @@ export default function MenuItemsPage() {
   return (
     <section className="mt-8">
       <UserTabs isAdmin={true}/>
+      <form className="mt-8 max-w-md mx-auto">
+        <div className="flex items-end gap-2">
+          <div className="grow">
+            <label>Menu item name</label>
+            <input type="text"/>
+          </div>
+          <div>
+            <button className="mb-2" type="submit">Create</button>
+          </div>
+        </div>
+      </form>
     </section>
   );
 }
